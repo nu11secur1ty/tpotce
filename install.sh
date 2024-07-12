db5f34699863aad18ba09a96aba94ff514eb83e0
nu11secur1ty <venvaropt@gmail.com>
2024-07-12 09:31:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -179,6 +179,7 @@ echo "### If you don't want to install the OPSEC-T-pot jus press Ctrl + C to qui
 while true; do
   #read -p "### Install Type? (h/s/m) " myTPOT_TYPE
   read -p "### Please press (H/h) to install OPSEC-T-pot" myTPOT_TYPE
+  echo
   case "${myTPOT_TYPE}" in
     h|H)
       echo
